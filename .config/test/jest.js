402fc107add3e10d1c6b6f9ccc6f0e36dff6d1d7
Erik Hughes <erik.hughes@evry.com>
2018-11-17 20:36:40 +0100

    test: finished test setup

--- .config/test/jest.js
+++ .config/test/jest.js
@@ -2,4 +2,11 @@ module.exports = {
   rootDir: '../../',
   preset: 'jest-preset-angular',
   setupTestFrameworkScriptFile: '<rootDir>/src/setupJest.ts',
+  globals: {
+    VERSION: new Date().toISOString(),
+    __TRANSFORM_HTML__: true,
+    'ts-jest': {
+      tsConfigFile: 'src/tsconfig.spec.json',
+    },
+  },
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routing';
+import { AppState } from './app.state';
+import { DebugComponent } from './debug/debug.component';
 
-test('renders markup to snapshot', () => {
-  const fixture = TestBed.createComponent(AppComponent);
-  expect(fixture).toMatchSnapshot();
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot(),
+        RouterTestingModule.withRoutes(APP_ROUTES),
+      ],
+      declarations: [AppComponent, DebugComponent],
+      providers: [AppState],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [DebugComponent],
+      },
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render markup that match snapshot', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should dynamically add the debug controller', () => {
+    const controller = fixture.debugElement.nativeElement.querySelector(
+      'c-debug',
+    );
+    const trigger = controller.querySelector('.trigger');
+    trigger.click();
+    fixture.detectChanges();
+    expect(controller.getAttribute('aria-expanded')).toBe('true');
+  });
 });
